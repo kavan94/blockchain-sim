bed6959a555cdc2893017b1cca33b563cb8d3bd8
Kavan <keghbalnia@gmail.com>
2019-01-11 18:44:01 -0500

    might as well create all of these in parallel
     - instead of sequentially, as the previous loop would wait for each promise to return before moving on

--- app/helpers/init.js
+++ app/helpers/init.js
@@ -3,9 +3,10 @@
 const account = require('../models/account')
 
 exports.createAccounts = async () => {
-	for (var i = 0; i < NUM_ACCOUNTS; i++) {
-		let newAccount = new account.Account(`Account ${i}`);
+	let accountNames = Array.from(new Array(NUM_ACCOUNTS), (val, index) => { return `Account ${index + 1}`});
+	await Promise.all(accountNames.map( async (name) => {
+		let newAccount = new account.Account(name);
 		await newAccount.initialize();
-		global.ACCOUNT_MAP[newAccount.publicKey] = newAccount;
-	}
+		ACCOUNT_MAP[newAccount.publicKey] = newAccount;
+	}))
 }

2b871c7efcafcf6ebb5883395d65520ca735ad4f
Kavan <keghbalnia@gmail.com>
2019-01-13 17:29:42 -0500

    same thing, better name

--- app/models/block.js
+++ app/models/block.js
@@ -5,14 +5,14 @@ exports.Block = class Block {
 	// there's no reason to use a DAG in this ultra-simplified simulation, because
 	// - no ASIC resistance desired
 	// - no "light" clients/nodes
-	constructor(transactions, beneficiary, number, timestamp, nonce, parentHash) {
+	constructor(transactions, miner, number, timestamp, nonce, parentHash) {
 		this.header = {
 			parentHash: parentHash,
 			nonce: nonce,
 			txHash: objectHash({ tx: this.transactions })
 		};
 		this.transactions = transactions;
-		this.beneficiary = beneficiary;
+		this.miner = miner;
 		this.number = number;
 		this.timestamp = timestamp;
 		this.parentHash = parentHash;

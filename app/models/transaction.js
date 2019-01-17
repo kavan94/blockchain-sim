4a07b2034b03182ff3bd999c9e843a17cad74b8f
Kavan <keghbalnia@gmail.com>
2019-01-16 21:54:39 -0500

    add nonce to tx

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -1,9 +1,10 @@
 exports.Transaction = class Transaction {
 
-	constructor(from, to, value) {
+	constructor(from, to, value, nonce) {
                 this.from = from;
                 this.to = to;
                 this.value = value;
                 this.timestamp = new Date();
+                this.nonce = nonce;
 	}
 }
